c36f59004171fc78407bd56640a09463eea44cf2
Micha Krause <micha@noris.net>
2015-07-24 14:57:30 +0200

    added subscribe to update-grub exec to only run if config file changed

--- manifests/update.pp
+++ manifests/update.pp
@@ -4,6 +4,7 @@ class grub2::update inherits grub2 {
   if $grub2::update_grub {
     exec { 'Update GRUB':
       command     => $grub2::update_binary,
+      subscribe   => File[$grub2::config_file],
       refreshonly => true,
     }
   }
